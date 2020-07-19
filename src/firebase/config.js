893966c7760bef9300d14d8c633d8abdcfe2dcb5
WeiMingGit <ongweiming96@gmail.com>
2020-07-20 01:37:37 +0800

    Updates

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAbaqHt-vqBqic4HRc8Po4x7DXs9t0fhjw',
-  authDomain: 'your-auth-domain-b1234.firebaseapp.com',
-  databaseURL: 'https://react-native-bcb74.firebaseio.com/',
-  projectId: 'react-native-bcb74',
-  storageBucket: 'your-project-id-1234.appspot.com',
-  messagingSenderId: '12345-insert-yourse',
-  appId: '1:1061628549130:android:7e8cd097d1abf7d93667f3',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 if (!firebase.apps.length) {
